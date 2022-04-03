6e073c48259006341e0559441dc208510fd83952
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2022-04-03 14:26:50 +0200

    usage process env for verification and page access tokens

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Imports dependencies and set up http server
 const
@@ -39,7 +40,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    let VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
       
     // Parse the query params
     let mode = req.query['hub.mode'];
